a3c9f4be86765d18b11f8da21c158e167e67f88e
otherdcn <danny.c.ntwari@gmail.com>
2024-06-18 16:36:14 +0200

    Re-organise and rename game.rb to tic_tac_toe.rb

--- tic_tac_toe/main.rb
+++ tic_tac_toe/main.rb
@@ -1,15 +1,4 @@
-require_relative "lib/game"
+require_relative "lib/tic_tac_toe"
 
-class TicTacToe
-  attr_reader :game
-  def initialize
-    @game = Game.new
-  end
-
-  def play
-    @game.play
-  end
-end
-
-tic_tac_toe = TicTacToe.new
+tic_tac_toe = TicTacToe::Game.new
 tic_tac_toe.play
