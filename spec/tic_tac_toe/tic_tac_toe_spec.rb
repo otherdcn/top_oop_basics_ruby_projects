f1803e32935e297e11c16298116edc0d78d453ec
otherdcn <danny.c.ntwari@gmail.com>
2024-10-21 17:52:33 +0200

    Add TicTacToe specs for #complete_round method

--- spec/tic_tac_toe/tic_tac_toe_spec.rb
+++ spec/tic_tac_toe/tic_tac_toe_spec.rb
@@ -1,7 +1,7 @@
 #require_relative '../tic_tac_toe/lib/tic_tac_toe'
 require_relative '../../tic_tac_toe/lib/tic_tac_toe'
 
-describe TicTacToe do
+describe TicTacToe::Game do
   describe "#initialize" do
     subject(:ttt_init) { TicTacToe::Game.new }
 
@@ -31,24 +31,47 @@ describe TicTacToe do
     end
 
     context "when rounds is 1" do
-      it "calls play_round once" do
-        expect(game_play).to receive(:play_round).once
+      it "calls complete_round once" do
+        expect(game_play).to receive(:complete_round).once
         game_play.play(1)
       end
     end
 
     context "when rounds is 3" do
-      it "calls play_round three times" do
-        expect(game_play).to receive(:play_round).exactly(3).times
+      it "calls complete_round three times" do
+        expect(game_play).to receive(:complete_round).exactly(3).times
         game_play.play(3)
       end
     end
 
     context "when rounds id 10" do
-      it "calls play_round ten times" do
-        expect(game_play).to receive(:play_round).exactly(10).times
+      it "calls complete_round ten times" do
+        expect(game_play).to receive(:complete_round).exactly(10).times
         game_play.play(10)
       end
+    end
+  end
+
+  describe "#complete_round" do
+    subject(:game_complete_round) { TicTacToe::Game.new }
+    let(:board_display) { game_complete_round.instance_variable_get(:@board) }
+
+    before do
+      allow(game_complete_round).to receive(:puts)
+      allow(game_complete_round).to receive(:reset_round_state)
+      allow(game_complete_round).to receive(:announce_round_winner)
+      allow(game_complete_round).to receive(:fill_board_per_turn)
+      allow(board_display).to receive(:combined_grids)
+    end
+
+    it "calls fill_board_per_turn 5 times for 3x3 board" do
+      allow(game_complete_round).to receive(:fill_board_per_turn).exactly(5).times
+      game_complete_round.complete_round(1)
+    end
+
+    it "sends combined_grids to board_display instance" do
+      expect(board_display).to receive(:combined_grids).once
+      game_complete_round.complete_round(1)
     end
   end
 end
