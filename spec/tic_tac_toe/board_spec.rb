2ad801d407520942e3f023c0a800d042a4de211f
otherdcn <danny.c.ntwari@gmail.com>
2024-10-22 20:33:11 +0200

    Add TicTacToe specs for Board#mark

--- spec/tic_tac_toe/board_spec.rb
+++ spec/tic_tac_toe/board_spec.rb
@@ -15,4 +15,28 @@ describe Board do
       expect(grid_attr && key_attr).to be_a(Array)
     end
   end
+
+  describe "#mark" do
+    subject(:board_obj) { described_class.new }
+    let(:player_board_mark) { "x" }
+    let(:coord_id) { "a" }
+    let(:coord) { [0, 0] }
+
+    before do
+      allow(board_obj).to receive(:grid=)
+    end
+
+    it "changes the multi-dimensioanl array element" do
+      board_obj.mark(player_board_mark, coord_id)
+
+      expect(board_obj.grid[coord[0]][coord[1]]).to eq("x")
+    end
+
+    it "returns the multi-dimensioanl array co-ordinates" do
+      return_coord = board_obj.mark(player_board_mark, coord_id)
+
+      expect(return_coord).to eq([0, 0])
+    end
+  end
 end
+
